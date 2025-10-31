a6a44cf49875cd716d0b348c7f1b2176ff6b5751
Laloo <lelisahailu22@gmail.com>
2025-10-31 10:03:27 +0300

    added styles.container

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -2,12 +2,12 @@ import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
 export default function Index() {
   return (
-<View>
-    <Text>Entry Screen - Awesome</Text>
-  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-  <Text style={styles.smallText}>ALX is awesome</Text>
-</View>
+    <View>
+      <Text style={styles.container}>Entry Screen - Awesome</Text>
+      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+      <Text style={styles.smallText}>ALX is awesome</Text>
+    </View>
   );
 }
 
